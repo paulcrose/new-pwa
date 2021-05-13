74124193814a795d4a44ff6b7599c3efec665405
paulcrose <61593103+paulcrose@users.noreply.github.com>
2021-05-13 15:07:21 -0400

    change name

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ function addUserName() {
     document.getElementById('currentname').innerHTML = username;
 }
 
+function changeName(){
+  username = document.getElementById("userNameSlot").value;
+  document.getElementById('currentname').innerHTML = username;
+  localStorage.setItem("name", document.getElementById("userNameSlot").value);
+  console.log(username);
+}
+
 
 
 window.onload = () => {
