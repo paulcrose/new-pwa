50a4d840f6cfff6eac4ae979145de55db7e0d628
paulcrose <61593103+paulcrose@users.noreply.github.com>
2021-05-13 11:39:42 -0400

    Revert "ok"
    This reverts commit 7708532c73884dd9ddfbdcb4c91205d8d8dcb441.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,8 @@ var username = "Bob";
 function changeVar(){
     username = document.getElementById("userNameSlot").value;
     document.getElementById('currentname').innerHTML = username;
-    localStorage.setItem("name", document.getElementById("userNameSlot").value);
     console.log(username);
 } 
-
-function updateVar(){
-  document.getElementById('currentname').innerHTML = localStorage.getItem('name');
-}
 
 
 window.onload = () => {
