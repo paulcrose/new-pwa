4cd3dad02269e68a03482d18ceb252733c3770f9
paulcrose <61593103+paulcrose@users.noreply.github.com>
2021-05-14 14:54:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-window.addEventListener('load', function() {
-  window.history.pushState({ noBackExitsApp: true }, '')
-})
-
-window.addEventListener('popstate', function(event) {
-  if (event.state && event.state.noBackExitsApp) {
-    window.history.pushState({ noBackExitsApp: true }, '')
-  }
-})
 
 var username; {
   if (localStorage.length === 0) {
